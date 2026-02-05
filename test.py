e481cf1301f9fe039de45a9a39e7f9c6ccae5344
D0WN3D <32816747+D0WN3D@users.noreply.github.com>
2026-02-06 01:03:19 +0200

    fix: test.py

--- test.py
+++ test.py
@@ -6,18 +6,14 @@ import unittest
 # gobyte block #1
 # moo@b1:~/.gobyte$ gobyted getblockhash 1
 # 00000c8a1ff01bae3f3875c81cb14115429af5744643b34b4ad1cbb7d2d59ca2
-# moo@b1:~/.gobyte$ gobyted getblock # 00000c8a1ff01bae3f3875c81cb14115429af5744643b34b4ad1cbb7d2d59ca2
-#{
+# moo@b1:~/.gobyte$ gobyted getblockheader 00000c8a1ff01bae3f3875c81cb14115429af5744643b34b4ad1cbb7d2d59ca2
+# {
 #   "hash": "00000c8a1ff01bae3f3875c81cb14115429af5744643b34b4ad1cbb7d2d59ca2",
-#   "confirmations": 1534987,
-#   "size": 179,
+#   "confirmations": 1535025,
 #   "height": 1,
 #   "version": 536870912,
 #   "versionHex": "20000000",
 #   "merkleroot": "a0d06cd65fd7feef3b4223cc926ec2b8320a0ddddf8779c6571ce169826dd58f",
-#   "tx": [
-#     "a0d06cd65fd7feef3b4223cc926ec2b8320a0ddddf8779c6571ce169826dd58f"
-#   ],
 #   "time": 1510848001,
 #   "mediantime": 1510848001,
 #   "nonce": 320604,
@@ -32,12 +28,12 @@ import unittest
 
 # gobyte block #1 info
 header_hex = (
-    "1209873605" +  # version (BE) // not sure
-    "07bcd769a4334e7b88480e9b9b03f792ae4c73141ab090dff85c05013b030000" +  # prev block (BE)
-    "8fd56d8269e11c57c67987dfdd0d0a32b8c26e92cc23423beffed75fd66cd0a0"  # merkle root (BE)
-    "01B60D5A" +  # time (to HEX then BE) //not sure
-    "f0ff0f1e" +  # bits (BE)
-    "5CE40400"    # nonce (to HEX then BE) //not sure
+    "00000020" +  # version (to HEX then LE) // not sure
+    "07bcd769a4334e7b88480e9b9b03f792ae4c73141ab090dff85c05013b030000" +  # prev block (32Bit LE)
+    "8fd56d8269e11c57c67987dfdd0d0a32b8c26e92cc23423beffed75fd66cd0a0"  # merkle root (32Bit LE)
+    "01960d5a" +  # time (to HEX then LE) //not sure
+    "f0ff0f1e" +  # bits (LE)
+    "5ce40400"    # nonce (to HEX then LE) //not sure
 )
 
 best_hash = "a29cd5d2b7cbd14a4bb3434674f59a421541b11cc875383fae1bf01f8a0c0000"
